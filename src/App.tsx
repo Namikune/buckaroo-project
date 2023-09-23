e5ecfadf9313593b34593b1e736459f770da0500
Nami <nami@Namis-MacBook-Air.local>
2023-09-23 22:33:00 +0200

    added React Calendar, soewhat of a mockup of the Book-Us page, CSS used is not good for production yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import './pages/navbar.tsx'
-import { Link } from 'react-router-dom';
-import NavBar from './pages/navbar.tsx';
-
-
+import NavBar from './components/navbar.tsx';
+import Calendar from './components/home/Calendar.tsx';
+import Home from './pages/home.tsx';
+import BookUs from './pages/book-us.tsx';
+import Pricing from './pages/pricing.tsx';
+import Contact from './pages/contact.tsx';
+import QA from './pages/qa.tsx';
+import LogIn from './pages/log-in.tsx';
 
 function App() {
-  
   return (
 
-<div className="font-bold">
-  <header className="sticky top-0 bg-gradient-to-r from-gray-700 to-black h-16 flex justify-between items-center px-4">
-    <div id="img-container" className="flex items-center">
-      <Link to='/home'>
-      <div className="bg-gradient-to-r from-gray-200 to-black p-2 rounded">
-        <img src="assets/Logo.png" alt="Logo" className="h-10"></img>
+  <main className="font-sans">
+      <div>
+      <NavBar />
       </div>
-      </Link>
-    </div>
+  <div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/BookUs' element={<BookUs />} />
+      <Route path='/Pricing' element={<Pricing />} />
+      <Route path='/Contact' element={<Contact />} />
+      <Route path='/QA' element={<QA />} />
+      <Route path='/LogIn' element={<LogIn />} />
+    </Routes>
+  </div>
+      
+  </main>
 
-    <div className="flex space-x-4 text-center">
-      <ul className="flex space-x-4">
-        <li><Link to='/bookus' className="font-bold text-gray-100 hover:text-white hover:shadow hover:shadow-md transition">Book Us</Link></li>
-        <li><Link to='/pricing' className="font-bold text-gray-100 hover:text-white hover:shadow hover:shadow-md transition">Pricing</Link></li>
-        <li><Link to='/contact' className="font-bold text-gray-100 hover:text-white hover:shadow hover:shadow-md transition">Contact</Link></li>
-        <li><Link to='/qa' className="font-bold text-gray-100 hover:text-white hover:shadow hover:shadow-md transition">QA</Link></li>
-        <li><Link to='/login' className="font-bold text-gray-100 hover:text-white hover:shadow hover:shadow-md transition">Log In</Link></li>
-      </ul>
-    </div>
-  </header>
-
-  <body className="flex justify-center items-center h-screen bg-gradient-to-r from-gray-300 to-gray-600">
-    <NavBar />
-  </body>
-</div>
-
-
-
-   
     );
   }
 
